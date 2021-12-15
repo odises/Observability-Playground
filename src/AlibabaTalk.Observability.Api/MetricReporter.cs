ada210d99cbdd115c0a1e668a34514c7b558e66c
Reza Ahmadi <r.odises@gmail.com>
2021-12-15 15:33:32 +0330

    Change Histogram buckets from Exponential to Linear

--- src/AlibabaTalk.Observability.Api/MetricReporter.cs
+++ src/AlibabaTalk.Observability.Api/MetricReporter.cs
@@ -22,7 +22,7 @@ namespace AlibabaTalk.Observability.Web
             _responseTimeHistogram = Metrics.CreateHistogram("request_duration_seconds",
                 "The duration in seconds between the response to a request.", new HistogramConfiguration
                 {
-                    Buckets = Histogram.ExponentialBuckets(0.01, 2, 10),
+                    Buckets = Histogram.LinearBuckets(0.02, 3, 10),
                     LabelNames = new[] { "status_code", "method" }
                 });
         }
